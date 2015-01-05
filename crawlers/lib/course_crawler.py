27960023e498b30694020f623557c7a215b1cd60
Utsav Narayan Singh <vastusingh@gmail.com>
2015-01-06 04:15:29 +0530

    Updated gitignore and included crawler code

---
+++ crawlers/lib/course_crawler.py
@@ -0,0 +1,68 @@
+__author__ = 'utsavnarayan'
+
+from bs4 import BeautifulSoup
+from crawlers import Course, CourseDetails
+import urllib2
+from collections import namedtuple
+
+CourseInfo = namedtuple('CourseDetails',
+                        ['short_url', 'name', 'instructor', 'summary', 'full_summary', 'syllabus', 'popularity'])
+
+
+_BASE_URL = "https://www.open2study.com"
+_COURSES_URL = _BASE_URL + '/courses/'
+
+
+def _fetch_course_info(course_url):
+    soup = BeautifulSoup(urllib2.urlopen(course_url))
+    return CourseInfo(short_url=soup.find('article')['about'],
+                      name=soup.find('h1').get_text(),
+                      instructor=soup.find('h5').get_text(),
+                      summary=soup.find('div', {"class": "summary"}).get_text(),
+                      full_summary=soup.find('div', {"class": "full-body"}).get_text(),
+                      syllabus=soup.find('div', {"class": "whatyouwilllearncontainer"}).get_text(),
+                      popularity=int(soup.find('div', {"class": "student-numbers-header"}).get_text().split(' ')[0]))
+
+
+def _save_course(url, short_url, name):
+    course = Course(url=url, short_url=short_url, name=name)
+    course.save()
+    return course
+
+
+def _save_course_details(course, short_url, instructor, summary, full_summary, syllabus, popularity):
+    course_details = CourseDetails(course=course,
+                                   short_url=short_url,
+                                   instructor=instructor,
+                                   summary=summary,
+                                   full_summary=full_summary,
+                                   syllabus=syllabus,
+                                   popularity=popularity)
+    course_details.save()
+
+
+def crawl_courses():
+    course_urls = []
+    soup = BeautifulSoup(urllib2.urlopen(_COURSES_URL))
+    for link in soup.find_all('a'):
+        try:
+            if '/node/' in link.get('href'):
+                course_urls.append(_BASE_URL + link.get('href'))
+        # Add a specific exception to catch here
+        except:
+            pass
+
+    for course_url in set(course_urls):
+        course_info = _fetch_course_info(course_url)
+
+        course = _save_course(_BASE_URL+course_info.short_url,
+                              course_info.short_url,
+                              course_info.name)
+
+        _save_course_details(course,
+                             course_info.short_url,
+                             course_info.instructor,
+                             course_info.summary,
+                             course_info.full_summary,
+                             course_info.syllabus,
+                             course_info.popularity)
